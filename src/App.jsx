0bd0b0a0a60e50fc413c75e30b90bb59b8c35e3e
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-01-15 14:44:46 +0000

    updated input and card details

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
+/* eslint react/prop-types: 0 */
+
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 
 export default function App() {
+  const [showThankyou, setShowThankyou] = useState(false);
+
+  const [fullName, setFullName] = useState("");
+  const [cardNumber, setCardNumber] = useState("");
+  const [expireMonth, setExpireMonth] = useState("");
+  const [expireYear, setExpireYear] = useState("");
+  const [cvc, setCvc] = useState();
+
   return (
     <div className="wrapper">
       <div className="component">
@@ -17,9 +28,13 @@ export default function App() {
                   className="card"
                 />
                 <div className="card--front__text-box">
-                  <p className="card__number">0000 0000 0000 0000</p>
-                  <p className="card__name">Jane Appleseed</p>
-                  <p className="card__date">00/00</p>
+                  <p className="card__number">
+                    {cardNumber || `0000 0000 0000 0000`}
+                  </p>
+                  <p className="card__name">{fullName || `Your name`}</p>
+                  <p className="card__date">
+                    {expireMonth || `00`}/{expireYear || `00`}
+                  </p>
                 </div>
               </div>
             </div>
@@ -27,19 +42,36 @@ export default function App() {
             <div className="card__back-position">
               <div className="card--back__box">
                 <img src="../public/images/bg-card-back.png" className="card" />
-                <p className="back__card-cvc">000</p>
+                <p className="back__card-cvc">{cvc || `000`}</p>
               </div>
             </div>
           </div>
 
-          <Form />
+          {!showThankyou && (
+            <Form
+              setShowThankyou={setShowThankyou}
+              setFullName={setFullName}
+              setCardNumber={setCardNumber}
+              setExpireMonth={setExpireMonth}
+              setExpireYear={setExpireYear}
+              setCvc={setCvc}
+            />
+          )}
+          {showThankyou && <ThankYou setShowThankyou={setShowThankyou} />}
         </main>
       </div>
     </div>
   );
 }
 
-function Form() {
+function Form({
+  setShowThankyou,
+  setFullName,
+  setCardNumber,
+  setExpireMonth,
+  setExpireYear,
+  setCvc,
+}) {
   const {
     register,
     handleSubmit,
@@ -54,7 +86,15 @@ function Form() {
     },
   });
 
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = (data) => {
+    setFullName(data.fullName);
+    setCardNumber(data.cardNumber);
+    setExpireMonth(data.expireMonth);
+    setExpireYear(data.expireYear);
+    setCvc(data.cvc);
+
+    setShowThankyou(true);
+  };
 
   return (
     <div>
@@ -90,13 +130,13 @@ function Form() {
             <label className="input__label">Exp. Date (MM/YY)</label>
             <div className="input__date">
               <input
-                className="input"
+                className="input input--small"
                 type="number"
                 placeholder="MM"
                 {...register("expireMonth", { required: true })}
               />
               <input
-                className="input"
+                className="input input--small"
                 type="number"
                 placeholder="YY"
                 {...register("expireYear", { required: true })}
@@ -108,10 +148,10 @@ function Form() {
               ))}
           </div>
 
-          <div className="input-control">
+          <div className="input-control input--cvc">
             <label className="input__label">CVC</label>
             <input
-              className="input"
+              className="input input--cvc-fix"
               type="number"
               placeholder="e.g. 123"
               {...register("cvc", { required: true })}
@@ -119,9 +159,23 @@ function Form() {
             {errors.cvc && <span className="input__error">Cant be blank</span>}
           </div>
         </div>
+        <button className="btn">Confirm</button>
       </form>
     </div>
   );
 }
 
-/*       e.g. 123 Confirm Thank you! We've added your card details Continue */
+function ThankYou({ setShowThankyou }) {
+  return (
+    <section className="thankyou__box">
+      <img src="../public/images/icon-complete.svg" className="thankyou__img" />
+      <h2 className="thankyou__heading">Thank you!</h2>
+      <p className="thankyou__text">We&apos;ve added your card details</p>
+      <button onClick={() => setShowThankyou(false)} className="btn">
+        Continue
+      </button>
+    </section>
+  );
+}
+
+/*   Continue */
